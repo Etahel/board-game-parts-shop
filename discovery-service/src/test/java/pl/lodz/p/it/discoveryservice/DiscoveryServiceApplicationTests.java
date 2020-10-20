d4ad02fb19b24db61591cb4d072013851d935a2a
Etahel <Sh3rL0ck#266415>
2020-10-20 18:59:37 +0200

    added base docker configuration

---
+++ discovery-service/src/test/java/pl/lodz/p/it/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
